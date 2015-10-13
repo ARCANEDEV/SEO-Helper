6e5e486f38a363214fad789f2fcc31c4977a552d
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-13 16:07:11 +0100

    Adding Open Graph Support

---
+++ src/Contracts/SeoOpenGraph.php
@@ -0,0 +1,12 @@
+<?php namespace Arcanedev\SeoHelper\Contracts;
+
+/**
+ * Interface  SeoOpenGraph
+ *
+ * @package   Arcanedev\SeoHelper\Contracts
+ * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+interface SeoOpenGraph extends Renderable
+{
+    //
+}
