d42ed94702953905615db28b81fbb7a2e5160c22
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-14 16:52:10 +0100

    Adding the SeoHelper helper function

--- helpers.php
+++ helpers.php
@@ -1,2 +1,12 @@
 <?php
-//
+
+if ( ! function_exists('seo_helper')) {
+    /**
+     * Get the SeoHelper instance.
+     *
+     * @return \Arcanedev\SeoHelper\Contracts\SeoHelper
+     */
+    function seo_helper() {
+        return app('arcanedev.seo-helper');
+    }
+}
