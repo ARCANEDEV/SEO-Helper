88fddf9e757c98ba6cea090dd5433b751bc86f52
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-14 15:18:26 +0100

    Adding InvalidTwitterCardException

---
+++ src/Exceptions/InvalidTwitterCardException.php
@@ -0,0 +1,9 @@
+<?php namespace Arcanedev\SeoHelper\Exceptions;
+
+/**
+ * Class     InvalidTwitterCardException
+ *
+ * @package  Arcanedev\SeoHelper\Exceptions
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class InvalidTwitterCardException extends InvalidArgumentException {}
