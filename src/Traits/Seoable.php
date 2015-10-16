eb58821b2a8176a34cadd00cfd391f0b90fee774
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-16 13:57:09 +0100

    Adding Seoable Trait

---
+++ src/Traits/Seoable.php
@@ -0,0 +1,12 @@
+<?php namespace Arcanedev\SeoHelper\Traits;
+
+/**
+ * Trait     Seoable
+ *
+ * @package  Arcanedev\SeoHelper\Traits
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+trait Seoable
+{
+    //
+}
