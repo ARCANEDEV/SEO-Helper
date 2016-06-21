b06afd6fc86e252a7d2883161636cc9c823b807c
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-06-21 00:18:35 +0000

    Updating the Google Analytics Entity
     * Updating the google analytics url.
     * Switching the visibility of `renderGoogleScript()` method to `protected`.

--- src/Entities/Analytics.php
+++ src/Entities/Analytics.php
@@ -97,18 +97,17 @@ class Analytics implements AnalyticsInterface
      *
      * @return string
      */
-    private function renderGoogleScript()
+    protected function renderGoogleScript()
     {
-        if (empty($this->google)) {
-            return '';
-        }
+        if (empty($this->google)) return '';
 
         return <<<EOT
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
     ga('create', '$this->google', 'auto');
     ga('send', 'pageview');
 </script>
