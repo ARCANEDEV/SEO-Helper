cd238e7d94f90f2626ae5dce60141ba8006981bf
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-01-31 15:41:39 +0000

    Cleaning the package

--- src/Facades/SeoMeta.php
+++ src/Facades/SeoMeta.php
@@ -1,5 +1,6 @@
 <?php namespace Arcanedev\SeoHelper\Facades;
 
+use Arcanedev\SeoHelper\Contracts\SeoMeta as SeoMetaContract;
 use Illuminate\Support\Facades\Facade;
 
 /**
@@ -15,5 +16,5 @@ class SeoMeta extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor() { return 'arcanedev.seo-helper.meta'; }
+    protected static function getFacadeAccessor() { return SeoMetaContract::class; }
 }
